50e004562e819a0cfabb794913e44f42adc54023
Mitch Gamburg <mitch.gamburg@gmail.com>
2021-11-07 15:13:58 -0500

    first commit

---
+++ background.js
@@ -0,0 +1,11 @@
+
+chrome.commands.onCommand.addListener((command) => {
+  if (command === 'recursive-search') {
+    // 1. open a pop-up window with a form
+    // 2. run the search
+    // 3. render results
+    // prototype: default search for 50%
+  } else {
+    throw 42;
+  }
+});
